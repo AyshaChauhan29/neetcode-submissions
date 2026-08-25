2a0986ded1995f83fea4652057b343394c9db61c
Aysha Chauhan <ayshachauhan29@gmail.com>
2026-08-25 14:20:10 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
+        int count = 0;
+        Arrays.sort(people);
+
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            if(people[low] + people[high] <= limit){
+                count++;
+                low++;
+                high--;
+            }
+            else {
+                count++;
+                high--;
+            }
+        }
+        return count;
+    }
+}
